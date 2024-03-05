758aacee8bac481f20db45daee4847bf0b199603
ABjnr <adeniyiabraham74@gmail.com>
2024-03-05 20:17:40 +0100

    Learning JavaScript contd.

--- JS/46_map.js
+++ JS/46_map.js
@@ -9,11 +9,24 @@ const store = new Map([
   ["Underwear", 50],
 ]);
 
+// TO ADD A PAIR TO OUR MAP WE USE THE .set
+store.set("Hat", 40);
+
+// TO DELETE A PAIR WE USE .delete
+store.delete("Hat", 40);
+
+// TO SEARCH FOR A KEY WITHING THE MAP USE .has and place in a console.log()
+console.log(store.has("Hat"));
+
+// USE THE .size TO GET THE TOTAL NUMBER OF PAIRS IN THE MAP
+console.log(store.size);
 let shoppingCart = 0;
 
-// EMPLOYING AN ARROW FUNCTION EXPRESSION
-store.forEach((value, key) => console.log(`${key} $${value}`));
+//USE .get TO BRING OUT AN ITEM
+shoppingCart += store.get("T-shirt");
+shoppingCart += store.get("Underwear");
+console.log(shoppingCart);
 
-if (1 == 1) {
-  alert("PHEW! WHAT A WORLD!");
-}
+// EMPLOYING AN ARROW FUNCTION EXPRESSION
+/* the keys are strings representing items (T-shirt, Jeans, Socks, Underwear), and the values are numbers representing their prices. */
+store.forEach((value, key) => console.log(`${key} $${value}`));
